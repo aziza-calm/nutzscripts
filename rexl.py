c865a136c7d5c8dba6dbf36e0fcd452fef24ed89
aziza-calm <aziza.kalmasova@phystech.edu>
2020-02-09 16:31:28 +0300

    pure python script for excel files rebasement

--- rexl.py
+++ rexl.py
@@ -1,4 +1,6 @@
 import openpyxl as xl
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
 
 wto = xl.load_workbook("to.xlsx")
 wfrom = xl.load_workbook("from.xlsx")
@@ -40,6 +42,40 @@ for row in wsfrom.iter_rows(min_row=2):
             wsto.cell(row=cell_from.row + 2, column=26, value=cell_from.value)
         if cell_from.column_letter == 'U': # industry
             wsto.cell(row=cell_from.row + 2, column=27, value=cell_from.value)
+        if cell_from.column_letter == 'I':
+            if str(cell_from.value).strip().endswith("mipt.ru"):
+                wsto.cell(row=cell_from.row + 2, column=9, value=cell_from.value)
+            elif str(cell_from.value).strip().endswith("phystech.edu"):
+                wsto.cell(row=cell_from.row + 2, column=8, value=cell_from.value)
+            else:
+                wsto.cell(row=cell_from.row + 2, column=11, value=cell_from.value)
+        if cell_from.column_letter == 'J':
+            if str(cell_from.value).strip().endswith("mipt.ru"):
+                wsto.cell(row=cell_from.row + 2, column=9, value=cell_from.value)
+            elif str(cell_from.value).strip().endswith("phystech.edu"):
+                wsto.cell(row=cell_from.row + 2, column=8, value=cell_from.value)
+            else:
+                wsto.cell(row=cell_from.row + 2, column=11, value=cell_from.value)
+        if cell_from.column_letter == 'K':
+            if str(cell_from.value).strip().endswith("mipt.ru"):
+                wsto.cell(row=cell_from.row + 2, column=9, value=cell_from.value)
+            elif str(cell_from.value).strip().endswith("phystech.edu"):
+                wsto.cell(row=cell_from.row + 2, column=8, value=cell_from.value)
+            else:
+                wsto.cell(row=cell_from.row + 2, column=11, value=cell_from.value)
+        if cell_from.column_letter == 'N': # telefonnummer
+            wsto.cell(row=cell_from.row + 2, column=12, value=cell_from.value)
+        if cell_from.column_letter == 'M': # telefonnummer extra
+            wsto.cell(row=cell_from.row + 2, column=13, value=cell_from.value)
+        if cell_from.column_letter == 'A': # post
+            wsto.cell(row=cell_from.row + 2, column=24, value=cell_from.value)
+        if cell_from.column_letter == 'E':
+            parse_result = morph.parse(cell_from.value)
+            if len(parse_result) > 0:
+                if parse_result[0].tag.gender == 'masc':
+                    wsto.cell(row=cell_from.row + 2, column=5, value="Мужской")
+                if parse_result[0].tag.gender == 'femn':
+                    wsto.cell(row=cell_from.row + 2, column=5, value="Женский")
         
 
 wto.save("result.xlsx")
